129564c1003e492c2cae2806518738a978707234
Andreas Neustifter <astifter@gmx.at>
2010-11-13 22:22:05 +0100

    Updated code according to pylint messages.
    This references gh-15.

--- ConfigData.py
+++ ConfigData.py
@@ -16,38 +16,73 @@
 # You should have received a copy of the GNU General Public License along with
 # PhotoImport. If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
+"""
+Contains class ConfigData.
+"""
 
 import wx
+import logging
 
 class ConfigData:
+    """
+    This class holds and stores all the configuration data for the PhotoImport
+    classes. This includes storing and reloading those values from a
+    configuration file.
+    """
 
     def __init__(self):
         self.srcrecursive = False
         self.srcvalue = ""
         self.destvalue = ""
         self.foldernamevalue = ""
-        self.renamefiles = False;
+        self.renamefiles = False
         self.filenamevalue = ""
-        self.Load()
+        self.load()
 
-    def Load(self):
-        config = wx.Config("PhotoImport")
-        if not config.HasGroup("ConfigFrame"):
+    def load(self):
+        """
+        This loads the configuration from the wx.Config facility and stores
+        these values in attributes.
+        """
+        try:
+            config = wx.Config("PhotoImport")
+            if not config.HasGroup("ConfigFrame"):
+                logging.error("Can not find configuration section ConfigFrame, aborting.")
+                return
+            config.SetPath("ConfigFrame")
+        except:
+            logging.error("Can not open configuration, aborting.")
             return
-        config.SetPath("ConfigFrame")
-        self.srcrecursive = config.ReadBool("Recursive", False)
-        self.srcvalue = config.Read("Source", "")
-        self.destvalue = config.Read("Destination", "")
-        self.foldernamevalue = config.Read("FolderPattern", "")
-        self.renamefiles = config.ReadBool("RenameFiles", False)
-        self.filenamevalue = config.Read("FilePattern", "")
 
-    def Save(self):
-        config = wx.Config("PhotoImport")
-        config.SetPath("ConfigFrame")
-        config.WriteBool("Recursive", self.srcrecursive)
-        config.Write("Source", self.srcvalue)
-        config.Write("Destination", self.destvalue)
-        config.Write("FolderPattern", self.foldernamevalue)
-        config.WriteBool("RenameFiles", self.renamefiles)
-        config.Write("FilePattern", self.filenamevalue)
+        try:
+            self.srcrecursive = config.ReadBool("Recursive", False)
+            self.srcvalue = config.Read("Source", "")
+            self.destvalue = config.Read("Destination", "")
+            self.foldernamevalue = config.Read("FolderPattern", "")
+            self.renamefiles = config.ReadBool("RenameFiles", False)
+            self.filenamevalue = config.Read("FilePattern", "")
+        except:
+            logging.error("Can not load configuration, aborting.")
+            return
+
+    def save(self):
+        """
+        This stores the configuration to the wx.Config.
+        """
+        try:
+            config = wx.Config("PhotoImport")
+            config.SetPath("ConfigFrame")
+        except:
+            logging.error("Can not open configuration, aborting.")
+            return
+
+        try:
+            config.WriteBool("Recursive", self.srcrecursive)
+            config.Write("Source", self.srcvalue)
+            config.Write("Destination", self.destvalue)
+            config.Write("FolderPattern", self.foldernamevalue)
+            config.WriteBool("RenameFiles", self.renamefiles)
+            config.Write("FilePattern", self.filenamevalue)
+        except:
+            logging.error("Can not save configuration, aborting.")
+            return
--- FileList.py
+++ FileList.py
@@ -16,6 +16,10 @@
 # You should have received a copy of the GNU General Public License along with
 # PhotoImport. If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
+"""
+This modules handles all the files.
+"""
+
 import glob
 import os
 import datetime
@@ -23,87 +27,99 @@ import shutil
 import pyexiv2
 
 class FileList:
+    """
+    This module reads a list of files, sorts the into "days" and asks the user
+    for a folder for each day. It then copies the files and optionally renames
+    them.
+    """
 
     def __init__ (self, config, parent):
 
         self.parent = parent
-        self.config = config;
+        self.config = config
         self.files = {}
 
         if self.config.srcrecursive:
             filelist = []
-            for (dirp,dirs,files) in os.walk(self.config.srcvalue + "/"):
-                for f in files:
-                    filelist.append(os.path.join(dirp,f));
+            for (dirp, dirs, files) in os.walk(self.config.srcvalue + "/"):
+                for filename in files:
+                    filelist.append(os.path.join(dirp, filename))
         else:
             filelist = glob.glob(self.config.srcvalue + "/*")
 
-        for f in filelist:
-            exif = pyexiv2.ImageMetadata(f)
+        for filename in filelist:
+            exif = pyexiv2.ImageMetadata(filename)
             try:
                 exif.read()
             except:
                 continue
 
             try:
-                fm = exif['Exif.Photo.DateTimeOriginal'].value
+                filedate = exif['Exif.Photo.DateTimeOriginal'].value
             except:
                 try:
-                    fm = exif['Exif.Photo.DateTime'].value
+                    filedate = exif['Exif.Photo.DateTime'].value
                 except:
-                    fs = os.stat(f)
-                    fm = datetime.datetime.fromtimestamp(fs.st_mtime)
+                    filestat = os.stat(filename)
+                    filedate = datetime.datetime.fromtimestamp(filestat.st_mtime)
 
-            if fm.hour < 6:
-                date = fm - datetime.timedelta(days=1)
+            if filedate.hour < 6:
+                date = filedate - datetime.timedelta(days=1)
             else:
-                date = fm
+                date = filedate
 
             datestr = date.strftime("%Y%m%d")
 
             if not self.files.has_key(datestr):
                 self.files[datestr] = {}
 
-            self.files[datestr][f] = date
+            self.files[datestr][filename] = date
 
-
-    def Filter(self):
+    def filter(self):
+        """
+        This removes all the files where the date already has a folder.
+        """
 
         for date in sorted(self.files.keys()):
 
-            destpath = self.config.foldernamevalue.replace("<date>", date);
-            destpath = destpath.replace("<name>", "*");
+            destpath = self.config.foldernamevalue.replace("<date>", date)
+            destpath = destpath.replace("<name>", "*")
 
             if len(glob.glob(self.config.destvalue + "/" + destpath)) > 0:
                 del self.files[date]
 
 
-    def ShowDialog(self):
+    def showdialog(self):
+        """ This shows the CopyDialog for each days. """
 
         for date in sorted(self.files.keys()):
             import CopyDialog
-            cf = CopyDialog.CopyDialog(self.parent, -1, "")
-            cf.SetConfig(self.config, date, self.files[date], self.Copy)
-            cf.ShowModal()
+            copydialog = CopyDialog.CopyDialog(self.parent, -1, "")
+            copydialog.setconfig(self.config, date, self.files[date], self.copy)
+            copydialog.ShowModal()
 
-    def Copy(self,date,folder):
+    def copy(self, date, folder):
+        """
+        This actually copies the files, they are optionally renamed. 
+        """
+
         dest = self.config.destvalue + "/" + folder
 
         print dest + "\n"
         os.mkdir(dest)
 
-        for (f, d) in self.files[date].items():
+        for (filename, filedate) in self.files[date].items():
             path = dest
             if self.config.renamefiles:
-                (root, ext) = os.path.splitext(f)
+                (root, ext) = os.path.splitext(filename)
                 ext = ext[1:]
-                filename = self.config.filenamevalue.replace("<date>",date)
-                filename = filename.replace("<time>",d.strftime("%H%M%S"))
-                filename = filename.replace("<ext>",ext)
+                filename = self.config.filenamevalue.replace("<date>", date)
+                filename = filename.replace("<time>", filedate.strftime("%H%M%S"))
+                filename = filename.replace("<ext>", ext)
                 path = os.path.join(path, filename)
 
-            print f + " " + path + "\n"
-            shutil.copy2(f,path)
+            print filename + " " + path + "\n"
+            shutil.copy2(filename, path)
 
     def __str__(self):
 
@@ -111,18 +127,7 @@ class FileList:
 
         for date in sorted(self.files.keys()):
             retval.append("date: " + date)
-            for f in self.files[date]:
-                retval.append(f)
+            for filename in self.files[date]:
+                retval.append(filename)
 
         return "\n".join(retval)
-
-
-if __name__ == "__main__":
-
-    fl = FileList("/media/EOS_DIGITAL/DCIM/100CANON", "/mnt/data/andi/fotos/sessions/", "<date>_<name>")
-    print fl
-
-    fl.Filter()
-    print fl
-
-    fl.ShowDialog()
